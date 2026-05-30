ebe9ed12b628e090d8efc7b95a2d0edb5a8705da
Gideon Tetteh Nyamedi <nyamedigideon1@gmail.com>
2026-05-30 20:24:20 +0000

    Submit Assignment 2

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials NGT.
+# Assignment Number: 2
+#
+# Name: Nyamedi Gideon Tetteh
+# STUDENT ID:2425402457
+# Email: 2425402457@live.gctu.edu.gh
+# Grader: : Augustus Buckman
+#
+# On my honor, Nyamedi Gideon Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
 
+def main(): # this is the main function
+    
+    
+    print("...NGT")
+    
+    print()
+    print("...NNNN....NNNN........GGGGGGGGGGGG........TTTTTTTTTTTT.....")
+    print("...NNNNN...NNNN........GGGGGGGGGGGG........TTTTTTTTTTTT.....")
+    print("...NNNNNN..NNNN........GGGG..................TTTTTT.........")
+    print("...NNNN.NN.NNNN........GGGG..................TTTTTT.........")
+    print("...NNNN..NNNNNN........GGGG..GGGGGG..........TTTTTT.........") 
+    print("...NNNN..NNNNNN........GGGG..GGGGGG..........TTTTTT.........")
+    print("...NNNN...NNNNN........GGGG......GG..........TTTTTT.........")
+    print("...NNNN....NNNN........GGGGGGGGGGGG..........TTTTTT.........")
+    print("...NNNN....NNNN...**...GGGGGGGGGGGG...**.....TTTTTT.......**")
+    print("...NNNN....NNNN...**...GGGGGGGGGGGG...**.....TTTTTT.......**")
+    print()
 
 
 main()
